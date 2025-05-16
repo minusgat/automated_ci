3488ad2f70fc3bab179a6c376e7e67413686a7ad
Carlos Hernández <carlos.hernandez@memphis.mx>
2025-05-16 11:38:52 -0600

    fix(text): fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hi Guys!'),
+          child: Text('Hi Guys!!'),
         ),
       ),
     );
